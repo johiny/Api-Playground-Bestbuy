0df9cbf5f2b89e5453b630e84e65b69c35525313
johiny <johiny7@live.com>
2022-07-06 13:13:15 -0500

    preparacion para deplegar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 'use strict';
-
+require('dotenv').config()
 const app = require('./app');
-const port = app.get('port');
+const port = process.env.PORT || app.get('port');
 const name = app.get('name');
 const server = app.listen(port);
 
-server.on('listening', () =>
+server.on('listening', () =>{
   console.log(`${name} started at http://${app.get('host')}:${port}`)
-);
+});
